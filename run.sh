79681be75bd7e8ef57528315caccd68259a40512
Han B. Jegal <jkhb717@gmail.com>
2025-03-10 13:46:08 -0500

    feature: you can check registers informations on qemu console

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -xue
 
-# QEMU file path
 QEMU=/opt/homebrew/bin/qemu-system-riscv32
 
-# Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+# clang 경로와 컴파일 옵션
+CC=/opt/homebrew/opt/llvm/bin/clang  # Ubuntu 등 환경에 따라 경로 조정: CC=clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# 커널 빌드
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf \
+    kernel.c
+
+# QEMU 실행
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot \
+    -kernel kernel.elf
